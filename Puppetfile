4a3280c4e310d2ed6e18f39acbe5536f88152e38
Trey Dockendorf <treydock@gmail.com>
2020-08-01 13:38:54 -0400

    Patroni is functional, but untested

--- Puppetfile
+++ Puppetfile
@@ -5,6 +5,7 @@ mod 'puppet/archive', '4.5.0'
 mod 'camptocamp/systemd', '2.9.0'
 mod 'richardc/datacat', '0.6.2'
 
+mod 'puppet/yum', '4.3.0'
 mod 'puppetlabs/postgresql', '6.6.0'
 mod 'jadestorm/patroni', '0.1.5'
 mod 'tailoredautomation/etcd', '0.3.0'
--- production/site_modules/profile/manifests/postgresql.pp
+++ production/site_modules/profile/manifests/postgresql.pp
@@ -31,6 +31,33 @@ class profile::postgresql (
       'initial-cluster-token'       => 'etcd-cluster-1',
       'initial-cluster'             => join($etcd_hosts, ','),
       'initial-cluster-state'       => 'new',
+      'enable-v2'                   => true,
     },
   }
+
+  yum::install { 'patroni':
+    source => 'https://github.com/cybertec-postgresql/patroni-packaging/releases/download/1.6.5-1/patroni-1.6.5-1.rhel7.x86_64.rpm',
+    before => Class['patroni'],
+  }
+  class { 'patroni':
+    scope                   => 'cluster',
+    use_etcd                => true,
+    pgsql_connect_address   => "${facts['networking']['fqdn']}:5432",
+    restapi_connect_address => "${facts['networking']['fqdn']}:8008",
+    pgsql_bin_dir           => '/usr/pgsql-9.6/bin',
+    pgsql_data_dir          => '/var/lib/pgsql/9.6/data',
+    pgsql_pgpass_path       => '/var/lib/pgsql/pgpass',
+    pgsql_parameters        => {
+      'max_connections' => 5000,
+    },
+    pgsql_pg_hba            => [
+      'host all all 0.0.0.0/0 md5',
+      'host replication repl 0.0.0.0/0 md5',
+    ],
+    superuser_username      => 'postgres',
+    superuser_password      => 'postgrespassword',
+    replication_username    => 'repl',
+    replication_password    => 'replpassword',
+  }
+  File[$patroni::config_path] ~> Service[$patroni::servicename]
 }
