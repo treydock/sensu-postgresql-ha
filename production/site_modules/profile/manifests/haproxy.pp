528c25ab8d34280f6cb4e6d5f23d4966384e247b
Trey Dockendorf <treydock@gmail.com>
2020-08-06 18:08:38 -0400

    Get HAProxy working with SSL client verification

--- production/site_modules/profile/manifests/haproxy.pp
+++ production/site_modules/profile/manifests/haproxy.pp
@@ -1,7 +1,23 @@
 class profile::haproxy {
+  file { '/etc/rsyslog.d/haproxy.conf':
+    content => join([
+      '$ModLoad imudp',
+      '$UDPServerAddress 127.0.0.1',
+      '$UDPServerRun 514',
+      'local0.* /var/log/haproxy.log',
+      '',
+    ], "\n"),
+    notify  => Exec['rsyslog-restart'],
+  }
+  exec { 'rsyslog-restart':
+    command     => '/bin/systemctl restart rsyslog',
+    refreshonly => true,
+  }
+
   class { 'haproxy':
     global_options => {
       maxconn => 100,
+      log     => '127.0.0.1:514 local0',
     },
     defaults_options => {
       log     => 'global',
@@ -14,6 +30,25 @@ class profile::haproxy {
         'check 5s',
       ],
     },
+  }
+
+  concat { '/etc/haproxy/haproxy.pem':
+   owner          => 'root',
+   group          => 'root',
+   mode           => '0600',
+   ensure_newline => true,
+   require        => Package['haproxy'],
+   notify         => Service['haproxy'],
+  }
+  concat::fragment { 'haproxy-ssl-cert':
+    target => '/etc/haproxy/haproxy.pem',
+    source => '/vagrant/ssl/haproxy.pem',
+    order  => '01',
+  }
+  concat::fragment { 'haproxy-ssl-key':
+    target => '/etc/haproxy/haproxy.pem',
+    source => '/vagrant/ssl/haproxy-key.pem',
+    order  => '02',
   }
 
   haproxy::listen { 'postgresql':
@@ -29,22 +64,23 @@ class profile::haproxy {
   haproxy::balancermember { 'psql1':
     listening_service => 'postgresql',
     server_names      => 'psql1',
-    ipaddresses       => '10.0.0.101',
+    ipaddresses       => 'psql1',
     ports             => '5432',
-    options           => 'maxconn 100 check port 8008 ssl ca-file /vagrant/ssl/ca.pem',
+    options           => 'maxconn 100 check port 8008 check-ssl ca-file /vagrant/ssl/ca.pem crt /etc/haproxy/haproxy.pem',
   }
   haproxy::balancermember { 'psql2':
     listening_service => 'postgresql',
     server_names      => 'psql2',
-    ipaddresses       => '10.0.0.102',
+    ipaddresses       => 'psql2',
     ports             => '5432',
-    options           => 'maxconn 100 check port 8008 ssl ca-file /vagrant/ssl/ca.pem',
+    options           => 'maxconn 100 check port 8008 check-ssl ca-file /vagrant/ssl/ca.pem crt /etc/haproxy/haproxy.pem',
   }
   haproxy::balancermember { 'psql3':
     listening_service => 'postgresql',
     server_names      => 'psql3',
-    ipaddresses       => '10.0.0.103',
+    ipaddresses       => 'psql3',
     ports             => '5432',
-    options           => 'maxconn 100 check port 8008 ssl ca-file /vagrant/ssl/ca.pem',
+    options           => 'maxconn 100 check port 8008 check-ssl ca-file /vagrant/ssl/ca.pem crt /etc/haproxy/haproxy.pem',
+
   }
 }
