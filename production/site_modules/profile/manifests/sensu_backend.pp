7647ed46dd80dd45193501d6786d2a7cd3b3f521
Trey Dockendorf <treydock@gmail.com>
2020-10-14 17:10:36 -0400

    Upgrade sensu module that has better support for postgresql ssl

--- production/site_modules/profile/manifests/sensu_backend.pp
+++ production/site_modules/profile/manifests/sensu_backend.pp
@@ -6,51 +6,15 @@ class profile::sensu_backend {
     backends => ['sensu-backend.example.com:8081'],
   }
   class { 'sensu::backend':
-    datastore            => 'postgresql',
-    manage_postgresql_db => false,
-    postgresql_host      => 'haproxy.example.com',
-    postgresql_port      => 5000,
-    postgresql_dbname    => 'sensu_events',
-    postgresql_user      => 'sensu',
-    postgresql_password  => 'sensu',
-  }
-  $dsn = "${sensu::backend::datastore::postgresql::dsn}?sslmode=require"
-  Sensu_postgres_config <| title == $sensu::backend::postgresql_name |> {
-    dsn => $dsn,
-  }
-  file { '/var/lib/sensu/.postgresql':
-    ensure  => 'directory',
-    owner   => 'sensu',
-    group   => 'sensu',
-    mode    => '0755',
-    require => Package['sensu-go-backend'],
-    notify  => Service['sensu-backend'],
-  }
-
-  file { '/var/lib/sensu/.postgresql/root.crt':
-    ensure => 'file',
-    source => $sensu::ssl_ca_source,
-    owner  => 'sensu',
-    group  => 'sensu',
-    mode   => '0644',
-    notify => Service['sensu-backend'],
-  }
-
-  file { '/var/lib/sensu/.postgresql/postgresql.crt':
-    ensure => 'file',
-    source => $sensu::backend::ssl_cert_source,
-    owner  => 'sensu',
-    group  => 'sensu',
-    mode   => '0644',
-    notify => Service['sensu-backend'],
-  }
-
-  file { '/var/lib/sensu/.postgresql/postgresql.key':
-    ensure => 'file',
-    source => $sensu::backend::ssl_key_source,
-    owner  => 'sensu',
-    group  => 'sensu',
-    mode   => '0600',
-    notify => Service['sensu-backend'],
+    datastore                  => 'postgresql',
+    manage_postgresql_db       => false,
+    postgresql_host            => 'haproxy.example.com',
+    postgresql_port            => 5000,
+    postgresql_dbname          => 'sensu_events',
+    postgresql_user            => 'sensu',
+    postgresql_password        => 'sensu',
+    postgresql_ssl_ca_source   => $sensu::ssl_ca_source,
+    postgresql_ssl_cert_source => $facts['puppet_hostcert'],
+    postgresql_ssl_key_source  => $facts['puppet_hostprivkey'],
   }
 }
